3a9345fa5fbf2de4ee545bff49d6b2ee058f59b3
Lucas Dota Trombini <119457135+Lucasdota@users.noreply.github.com>
2024-02-01 11:56:21 -0300

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Título from "@/components/home/Título";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 text-neutral-700 gap-10">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 text-neutral-700 gap-10 xs:p-12">
 			{/* título */}
 			<Título />
 			<Login />
--- src/components/home/Login.tsx
+++ src/components/home/Login.tsx
@@ -9,7 +9,7 @@ const Login = (props: Props) => {
 	return (
     <section
       aria-label="area de login"
-      className="flex flex-col items-center justify-center p-6 bg-white rounded shadow-lg gap-6 w-96 sm:w-72"
+      className="flex flex-col items-center justify-center p-6 bg-white rounded shadow-lg gap-6 w-96 xs:w-72"
     >
       <Form />
       <OtherBtns />
